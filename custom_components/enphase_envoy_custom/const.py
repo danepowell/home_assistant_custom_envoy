ebddf217964b1aa14e8de101713d13cf92491367
Dane Powell <git@danepowell.com>
2024-02-07 13:12:32 -0800

    Deprecated units

--- custom_components/enphase_envoy_custom/const.py
+++ custom_components/enphase_envoy_custom/const.py
@@ -10,7 +10,7 @@ from homeassistant.components.sensor import (
     SensorEntityDescription,
     SensorStateClass,
 )
-from homeassistant.const import ENERGY_WATT_HOUR, POWER_WATT, Platform, PERCENTAGE
+from homeassistant.const import UnitOfEnergy, UnitOfPower, Platform, PERCENTAGE
 
 DOMAIN = "enphase_envoy"
 
@@ -28,61 +28,61 @@ SENSORS = (
     SensorEntityDescription(
         key="production",
         name="Current Power Production",
-        native_unit_of_measurement=POWER_WATT,
+        native_unit_of_measurement=UnitOfPower.WATT,
         state_class=SensorStateClass.MEASUREMENT,
     ),
     SensorEntityDescription(
         key="daily_production",
         name="Today's Energy Production",
-        native_unit_of_measurement=ENERGY_WATT_HOUR,
+        native_unit_of_measurement=UnitOfEnergy.WATT_HOUR,
         state_class=SensorStateClass.TOTAL_INCREASING,
         device_class=SensorDeviceClass.ENERGY,
     ),
     SensorEntityDescription(
         key="seven_days_production",
         name="Last Seven Days Energy Production",
-        native_unit_of_measurement=ENERGY_WATT_HOUR,
+        native_unit_of_measurement=UnitOfEnergy.WATT_HOUR,
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.ENERGY,
     ),
     SensorEntityDescription(
         key="lifetime_production",
         name="Lifetime Energy Production",
-        native_unit_of_measurement=ENERGY_WATT_HOUR,
+        native_unit_of_measurement=UnitOfEnergy.WATT_HOUR,
         state_class=SensorStateClass.TOTAL_INCREASING,
         device_class=SensorDeviceClass.ENERGY,
     ),
     SensorEntityDescription(
         key="consumption",
         name="Current Power Consumption",
-        native_unit_of_measurement=POWER_WATT,
+        native_unit_of_measurement=UnitOfPower.WATT,
         state_class=SensorStateClass.MEASUREMENT,
     ),
     SensorEntityDescription(
         key="daily_consumption",
         name="Today's Energy Consumption",
-        native_unit_of_measurement=ENERGY_WATT_HOUR,
+        native_unit_of_measurement=UnitOfEnergy.WATT_HOUR,
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.ENERGY,
     ),
     SensorEntityDescription(
         key="seven_days_consumption",
         name="Last Seven Days Energy Consumption",
-        native_unit_of_measurement=ENERGY_WATT_HOUR,
+        native_unit_of_measurement=UnitOfEnergy.WATT_HOUR,
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.ENERGY,
     ),
     SensorEntityDescription(
         key="lifetime_consumption",
         name="Lifetime Energy Consumption",
-        native_unit_of_measurement=ENERGY_WATT_HOUR,
+        native_unit_of_measurement=UnitOfEnergy.WATT_HOUR,
         state_class=SensorStateClass.TOTAL_INCREASING,
         device_class=SensorDeviceClass.ENERGY,
     ),
     SensorEntityDescription(
         key="inverters",
         name="Inverter",
-        native_unit_of_measurement=POWER_WATT,
+        native_unit_of_measurement=UnitOfPower.WATT,
         state_class=SensorStateClass.MEASUREMENT,
     ),
     SensorEntityDescription(
@@ -101,21 +101,21 @@ SENSORS = (
     SensorEntityDescription(
         key="current_battery_capacity",
         name="Current Battery Capacity",
-        native_unit_of_measurement=ENERGY_WATT_HOUR,
+        native_unit_of_measurement=UnitOfEnergy.WATT_HOUR,
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.ENERGY
     ),
     SensorEntityDescription(
         key="grid_import",
         name="Total Grid Energy Imported",
-        native_unit_of_measurement=ENERGY_WATT_HOUR,
+        native_unit_of_measurement=UnitOfEnergy.WATT_HOUR,
         state_class=SensorStateClass.TOTAL_INCREASING,
         device_class=SensorDeviceClass.ENERGY,
     ),
     SensorEntityDescription(
         key="grid_export",
         name="Total Grid Energy Exported",
-        native_unit_of_measurement=ENERGY_WATT_HOUR,
+        native_unit_of_measurement=UnitOfEnergy.WATT_HOUR,
         state_class=SensorStateClass.TOTAL_INCREASING,
         device_class=SensorDeviceClass.ENERGY,
     ),
@@ -124,7 +124,7 @@ SENSORS = (
 BATTERY_ENERGY_DISCHARGED_SENSOR = SensorEntityDescription(
     key="battery_energy_discharged",
     name="Battery Energy Discharged",
-    native_unit_of_measurement=ENERGY_WATT_HOUR,
+    native_unit_of_measurement=UnitOfEnergy.WATT_HOUR,
     state_class=SensorStateClass.TOTAL,
     device_class=SensorDeviceClass.ENERGY
 )
@@ -132,7 +132,7 @@ BATTERY_ENERGY_DISCHARGED_SENSOR = SensorEntityDescription(
 BATTERY_ENERGY_CHARGED_SENSOR = SensorEntityDescription(
     key="battery_energy_charged",
     name="Battery Energy Charged",
-    native_unit_of_measurement=ENERGY_WATT_HOUR,
+    native_unit_of_measurement=UnitOfEnergy.WATT_HOUR,
     state_class=SensorStateClass.TOTAL,
     device_class=SensorDeviceClass.ENERGY
 )
